2058efe72a8a4dca79acde7757838568a515531a
Muskotti <toni.vanttinen@dnainternet.net>
2018-06-26 15:57:09 +0300

    Player movement and camera follow

---
+++ QuestOfSotalokki/Assets/Scripts/CameraFollowOverworld.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowOverworld : MonoBehaviour {
+
+    public Transform target;
+
+    private Vector3 offset;
+
+    public float smoothFactor = 0.9f;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate () {
+        Vector3 newPos = target.position + offset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+    }
+}
---
+++ QuestOfSotalokki/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Rigidbody rb;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+	}
+
+    private void Move()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+        rb.velocity = moveVelocity;
+    }
+}
