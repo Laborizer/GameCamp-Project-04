34f051aa0ebeafcfed287c55f2a36b4728a11266
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-06-26 15:24:17 +0300

    Camera switch
    Made camera switch between overworld and battle cameras

---
+++ QuestOfSotalokki/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+    public bool inBattle;
+
+    public Camera overworldCam;
+    public Camera battleCam;
+
+    // Use this for initialization
+    void Start () {
+        inBattle = false;
+        overworldCam.enabled = true;
+        battleCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkIfBattle();
+	}
+
+    private void checkIfBattle()
+    {
+        if(inBattle)
+        {
+            overworldCam.enabled = false;
+            battleCam.enabled = true;
+        }
+        else
+        {
+            overworldCam.enabled = true;
+            battleCam.enabled = false;
+        }
+    }
+}
