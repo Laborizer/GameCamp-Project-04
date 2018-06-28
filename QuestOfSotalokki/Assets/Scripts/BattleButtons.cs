6d1f2ec744c817292fc6017189f963cc0810c574
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-06-28 12:16:06 +0300

    Improved battle menus
    Made the battle menus better

---
+++ QuestOfSotalokki/Assets/Scripts/BattleButtons.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleButtons : MonoBehaviour {
+
+    public GameObject attack;
+    public GameObject specialMenu;
+    public GameObject itemMenu;
+
+    // Use this for initialization
+    void Start () {
+        specialMenu.SetActive(false);
+        itemMenu.SetActive(false);
+    }
+
+    public void enableSpecialWindow()
+    {
+        specialMenu.SetActive(true);
+        itemMenu.SetActive(false);
+    }
+
+    public void enableItemWindow()
+    {
+        itemMenu.SetActive(true);
+        specialMenu.SetActive(false);
+    }
+
+    public void doAttack()
+    {
+        specialMenu.SetActive(false);
+        itemMenu.SetActive(false);
+    }
+}
---
+++ QuestOfSotalokki/Assets/Scripts/ItemMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemMenu : MonoBehaviour {
+
+    public Button healthPotionButton;
+    public Button manaPotionButton;
+    public Button attackPotionButton;
+    public Button defensePotionButton;
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        healthPotionButton.GetComponentInChildren<Text>().text = "Health potion x " + player.GetComponent<Player>().healtPotionCount;
+        manaPotionButton.GetComponentInChildren<Text>().text = "Mana potion x ";
+        attackPotionButton.GetComponentInChildren<Text>().text = "Attack potion x ";
+        defensePotionButton.GetComponentInChildren<Text>().text = "Defense potion x ";
+    }
+}
