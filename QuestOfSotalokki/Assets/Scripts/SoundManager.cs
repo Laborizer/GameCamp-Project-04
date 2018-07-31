5aabf423b28b6cc6efc768bbe7f68de1f84e5e78
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-07-31 14:06:29 +0300

    Sounds
    More sounds and soundmanager script

---
+++ QuestOfSotalokki/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    AudioSource audiosource;
+
+    public AudioClip button;
+    public AudioClip enemyTalk;
+    public AudioClip npcTalk;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    public void buttonSoundPlay()
+    {
+        audiosource.PlayOneShot(button, 1f);
+    }
+
+    public void enemyTalkPlay()
+    {
+        audiosource.PlayOneShot(enemyTalk, 1f);
+    }
+
+    public void npcTalkPlay()
+    {
+        audiosource.PlayOneShot(npcTalk, 1f);
+    }
+}
