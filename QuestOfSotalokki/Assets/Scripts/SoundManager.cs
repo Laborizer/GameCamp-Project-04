bd2149a248e5a21f5d65fa44eea80a52c69e8e64
mitzuh <36733151+mitzuh@users.noreply.github.com>
2018-08-02 13:58:26 +0300

    Sound effect fix
    Fixed all sound effects changing pitch

--- QuestOfSotalokki/Assets/Scripts/SoundManager.cs
+++ QuestOfSotalokki/Assets/Scripts/SoundManager.cs
@@ -44,22 +44,27 @@ public class SoundManager : MonoBehaviour {
     // sound effects
     public void buttonSoundPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(button, 2f);
     }
     public void enemyTalkPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(enemyTalk, 3f);
     }
     public void npcTalkPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(npcTalk, 3f);
     }
     public void potionPickupPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(potionPickup, 3f);
     }
     public void specialLearnPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(specialLearn, 3f);
     }
     public void walkPlay()
@@ -73,28 +78,34 @@ public class SoundManager : MonoBehaviour {
     }
     public void potionDrinkPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(gulp, 15f);
     }
 
     // skill sound effects
     public void shotgunPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(shotgun, 2f);
     }
     public void firePlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(fire, 2f);
     }
     public void thunderPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(thunder, 2f);
     }
     public void icePlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(ice, 5f);
     }
     public void hitPlay()
     {
+        audiosource.pitch = 1f;
         audiosource.PlayOneShot(hit, 3f);
     }
 }
